7bc0d09c6bf742154431dea121b62ae52333fc59
Piotrek Stapp <piotr.stapp@allegro.pl>
2020-02-26 12:51:18 +0100

    Init commit

---
+++ src/IO.Swagger/Filters/BasePathFilter.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+using System.Text.RegularExpressions;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace IO.Swagger.Filters
+{
+    /// <summary>
+    /// BasePath Document Filter sets BasePath property of Swagger and removes it from the individual URL paths
+    /// </summary>
+   
+}
---
+++ src/IO.Swagger/Filters/GeneratePathParamsValidationFilter.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace IO.Swagger.Filters
+{
+    /// <summary>
+    /// Path Parameter Validation Rules Filter
+    /// </summary>
+    
+}
+
